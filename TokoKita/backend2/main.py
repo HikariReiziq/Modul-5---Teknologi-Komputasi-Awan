ab2f3dfed9d47f722a5c07b186ba8415a1f9747c
M. Hikari Reiziq Rakhadinta <reiziqzip@gmail.com>
2026-05-28 22:35:21 +0700

    first commit

---
+++ TokoKita/backend2/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f"Server 2 - TokoKita | Hostname: {socket.gethostname()}"
+
+@app.route('/products')
+def products():
+    data = [
+        {"id": 1, "name": "Laptop", "price": 12000000},
+        {"id": 2, "name": "Mouse", "price": 150000},
+        {"id": 3, "name": "Keyboard", "price": 350000}
+    ]
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ script.sh
@@ -0,0 +1,19 @@
+# Soal 1
+
+# Sub-Soal 1
+# Membuat struktur Folder
+
+# Sub-Soal 2
+# Mengisi ketiga file di folder backend1 dan folder backend2
+
+# Sub-Soal 3
+# Mengisi file nginx.conf, Dockerfile dan docker-compose.yml di folder nginx
+
+#pastikan posisimu berada di dalam folder nginx/
+docker-compose up --build
+
+# Sub-Soal 4
+# pastikan docker-compose.yml
+
+# Sub-Soal 5
+# Uji di url localhost, sama cek /products
